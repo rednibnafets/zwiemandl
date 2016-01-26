12aa6cbe89a4f73c73d87a37f814d1b945723cb1
stefan <stefan@brunhilde>
2016-01-26 13:40:31 +0000

    04.3 - some WidgetTests added

--- src/accounts/SilverAccount.java
+++ src/accounts/SilverAccount.java
@@ -3,9 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package accounts;
-
-import accounts.BaseAccount;
+package zwiemandl.src.accounts;
 
 /**
  *
