2ad16ba973203d9a8d5f2121e250f59d0a970cf7
stefan <stefan@brunhilde>
2016-01-26 14:21:47 +0000

    05 - Account factory

--- src/factory/AccountFactory.java
+++ src/factory/AccountFactory.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package zwiemandl.src.factory;
 
-/**
- *
- * @author stefan
- */
 public class AccountFactory {
     public static AccountI createAccount (String actype) {
         if (actype.equalsIgnoreCase("Silver")){
