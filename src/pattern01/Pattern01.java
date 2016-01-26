d375a7f7b4cf3aebee49b9d5c60df8b759d55c7a
stefan <stefan@silverdrop.trillium.lan>
2016-01-26 18:35:38 +0000

    06 try/catch block

--- src/pattern01/Pattern01.java
+++ src/pattern01/Pattern01.java
@@ -12,67 +12,73 @@ import zwiemandl.src.factory.AccountFactory;
 import zwiemandl.src.factory.AccountI;
 
 /**
- * Design Patterns: Factory 
+ * Design Patterns: Factory
+ *
  * @author stefan
  */
 public class Pattern01 {
 
     public static void main(String[] args) {
-        
+
         // one class, client uses new
-        oneClass() ;
+        oneClass();
         System.out.println("--------------------------------------");
         // use superclass and subclasses, client uses new
         superAndSubClasses();
         System.out.println("--------------------------------------");
         // use the Factory pattern -> do not allow the client to use new
-        
-        AccountI silver = AccountFactory.createAccount("Silver") ;
-        System.out.println( silver.rewardPoints(123.00) );
 
-        AccountI thegolden = AccountFactory.createAccount("GOLD") ;
-        System.out.println( thegolden.rewardPoints(123.00) );    
-        
-        AccountI anotherPlatinum = AccountFactory.createAccount("pLATINUM") ;
-        System.out.println( anotherPlatinum.rewardPoints(123.00) );    
-        
-        //AccountI bogusAccount = AccountFactory.createAccount("I throw an exception") ;        
-        
-    }
-    
-    public static void oneClass()
-    {
-        Account ac01 = new Account("silver") ;
-        ac01.rewardPoints(123.00) ;
-        System.out.println(ac01.getRewardPoints()) ;
+        try {
 
-        Account ac02 = new Account("gold") ;
-        ac02.rewardPoints(123.00) ;
-        System.out.println(ac02.getRewardPoints()) ;
+            AccountI silver = AccountFactory.createAccount("Silver");
+            System.out.println(silver.rewardPoints(123.00));
+
+            AccountI thegolden = AccountFactory.createAccount("GOLD");
+            System.out.println(thegolden.rewardPoints(123.00));
+
+            AccountI anotherPlatinum = AccountFactory.createAccount("pLATINUM");
+            System.out.println(anotherPlatinum.rewardPoints(123.00));
+
+            // makes the program crash if the exception is not handled
+            AccountI bogusAccount = AccountFactory.createAccount("I throw an exception");
+
+        } catch (IllegalArgumentException e) {
+            System.err.println("IndexOutOfBoundsException: " + e.getMessage());
+        }
         
-        Account ac03 = new Account("platinum") ;
-        ac03.rewardPoints(123.00) ;
-        System.out.println(ac03.getRewardPoints()) ; 
+        System.out.println("------- end of program --------") ;
     }
-    
-    public static void superAndSubClasses()
-    {
-        
-        SilverAccount ac001 = new SilverAccount() ;
-        ac001.rewardPoints(123.00) ;
-        System.out.println(ac001.getRewardPoints()) ;     
-        
-        GoldAccount ac002 = new GoldAccount() ;
-        ac002.rewardPoints(123.00) ;
-        System.out.println(ac002.getRewardPoints()) ;     
-        
-        PlatinumAccount ac003 = new PlatinumAccount() ;
-        ac003.rewardPoints(123.00) ;
-        System.out.println(ac003.getRewardPoints()) ;    
-        
+
+    public static void oneClass() {
+        Account ac01 = new Account("silver");
+        ac01.rewardPoints(123.00);
+        System.out.println(ac01.getRewardPoints());
+
+        Account ac02 = new Account("gold");
+        ac02.rewardPoints(123.00);
+        System.out.println(ac02.getRewardPoints());
+
+        Account ac03 = new Account("platinum");
+        ac03.rewardPoints(123.00);
+        System.out.println(ac03.getRewardPoints());
+    }
+
+    public static void superAndSubClasses() {
+
+        SilverAccount ac001 = new SilverAccount();
+        ac001.rewardPoints(123.00);
+        System.out.println(ac001.getRewardPoints());
+
+        GoldAccount ac002 = new GoldAccount();
+        ac002.rewardPoints(123.00);
+        System.out.println(ac002.getRewardPoints());
+
+        PlatinumAccount ac003 = new PlatinumAccount();
+        ac003.rewardPoints(123.00);
+        System.out.println(ac003.getRewardPoints());
+
         // if this is possible, use protected
         //System.out.println(ac003.balance) ;   
-    
     }
-    
+
 }
