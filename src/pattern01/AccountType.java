0a08951063fe247290f9f81ae77c8d287c37b284
stefan <stefan@silverdrop.trillium.lan>
2016-01-26 06:48:25 +0000

    01.1 - switch (one class)

---
+++ src/pattern01/AccountType.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pattern01;
+
+/**
+ *
+ * @author stefan
+ */
+public class AccountType {
+    
+    private String actype ;
+    
+    public AccountType(String type){
+        this.actype = type ;
+    }
+    
+}
